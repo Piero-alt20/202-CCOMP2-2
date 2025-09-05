a48ead80ce51a55104b9857184c9ea10568b2e37
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-09-05 08:23:49 -0500

    5-09

---
+++ semana4/nropalindrono/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /** Escribir un programa que pida un numero (3 a 9 digitos)
+    * y evalue si dicho numero es un palindromo.
+    *ejm
+       13331 -> es palindromo
+       2133214 -> no es palindromo
+       31311313 -> es palindromo
+       usando while y for todos y primos y n primos puro for
+       */
+       cout << "Escriba un numero de 3 a 9 digitos: " << endl;
+       cin>>n;
+
+}
--- semana4/numeroprimo/main.cpp
+++ semana4/numeroprimo/main.cpp
@@ -8,23 +8,20 @@ int main()
     *implemente un programa que solicite un numero por teclado
     *e indique si dicho numero es un numero primo"
     */
-    int n;
-    cout << "ingrese un numero: " << endl;
-    cin << n;
-    while ()
+ int n, i=2, d=0;
+    cin >> n;
+    while(i*i <= n){
+        if(n % 1 == 0){
+            d++;
+        }
+        if(d>0) break;
+        i++;
+    }
+    cout << ((d==0||n==2) ? "primo" : "no primo");
 
     /**
     *implemente un programa que solicite un numero por teclado
     es imprima los numeros primos menores que dicho numero
     */
-    int n, i=1, d=0;
-    cin >> n;
-    while(i <= n){
-        if(n % 1 == 0){
-            d++;
-        }
-        i++;
-        if ()
-    }
 
 }
