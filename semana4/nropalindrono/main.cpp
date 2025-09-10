ac8b931da3ae694210148422e8d08ef62d499111
ALUMNO-B302 <ALUMNO-B302@UCSP.AULAB>
2025-09-10 08:26:38 -0500

    10/09

--- semana4/nropalindrono/main.cpp
+++ semana4/nropalindrono/main.cpp
@@ -12,7 +12,20 @@ int main()
        31311313 -> es palindromo
        usando while y for todos y primos y n primos puro for
        */
+       int num(0);
        cout << "Escriba un numero de 3 a 9 digitos: " << endl;
-       cin>>n;
-
+       cin>>num;
+       int temp=num;
+       int invertido= 0;
+       while (temp>0){
+        invertido=(invertido*10) + (temp % 10);
+        temp/=10;
+       }
+       if (num==invertido){
+        cout << "es palindromo" << endl;
+       }
+       else{
+        cout << "no es palindromo" << endl;
+       }
+        return 0;
 }
---
+++ semana5/Funciones/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int potencia(int base, int exp){
+    int result=1;
+    //for (inicializacion;condicion;incremento)
+    for(int i=0; i<n; i++) {
+        result=result*base;
+ }
+int main()
+{
+    cout << potencia(3,4) << endl;
+    return 0;
+}
