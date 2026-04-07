45e25bb56eb143208e352fa171f045bd6bfd1bbf
Piero-alt20 <piero.apaza.viza@ucsp.edu.pe>
2026-04-07 13:00:57 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
-/*ingresa 10 numeros por teclado al array y muestra el promedio*/
-/*crea una funcion que reciba un array de 10 y que muestro el mayor y el menor*/
-/*crea una funcion que reciba un arreglo y que esta funcion lo invierta*/
+
 using namespace std;
+/*1-.cree una funcion que se llame duplicar que reciba la direccion de una variable y calcule el doble con punteros
+     &=direccion *=valor
+  2-.cree una funcion que reciba dos valores que se llame suma y sume los valores y lo muestra
+  3-. halla el mayor y contar los pares numero de un arreglo de 5 con punteros*/
 
-invertir(int arr[], int n){
-    cout <<"[";
-    for(int i=n-1;i>=0;i--)
-        cout << arr[i];
-    cout << "]";
-}
+/*void duplicar(int* a){
+    *a=*a*2;
+}*/
 
+/*void sumar(int *a, int *b){
+  int suma;
+  suma=*a+*b;
+  cout << suma<< endl;
+}*/
 int main()
 {
-    /*int numero[10];
-    int suma=0;
-    for(int i; i<10; i++){
-        cout << "ingresa un valor"<<endl;
-        cin>>numero[i];
-        suma+=numero[i];
-    }
-    cout << suma%10 << endl;
-*/
+    /*int a=7;
+    int b=8;
+    int c;
 
-    int numero[5];
-    for(int i; i<5; i++){
-        cout << "ingresa un valor"<<endl;
-        cin>>numero[i];
+    int *p=&a;
+    int *m=&b;
+
+    c=*p;
+    *p=*m;
+    *m=c;
+
+    cout << a << endl;
+    cout << b << endl;*/
+
+    /*int a=5;
+    duplicar(&a);
+    cout << a;*/
+
+    /*int a=10;
+    int b=20;
+
+    sumar(&a,&b);*/
+
+    int numero[5]{2,3,4,5,6};
+    int *p=numero;
+    int mayor;
+    int cont=0;
+
+    for(int i=0; i<5;i++){
+        if(*p%2==0){cont++;}
+
+        p++;
     }
-    invertir(numero,5);
+    cout<<cont;
     return 0;
 }
